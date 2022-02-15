f6d0f469d98a3646f19a2156b8efec904a498c05
heamanth-11 <heamanthofficial@gmail.com>
2022-02-15 11:36:18 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+<Header/>
+<AddNote @add-note="addNote"/>
+<Note @delete-note="deleteNote" :data= "data"/>
+</template>
+
+<script>
+import Note from './components/Note'
+import Header from './components/Header'
+import AddNote from './components/AddNote'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    AddNote,
+    Note
+  },
+  data () {
+    return {
+      data: []
+    }
+  },
+  methods: {
+    deleteNote (id) {
+      this.data = this.data.filter((data) => data.id !== id)
+    },
+    addNote (note) {
+      note.id = Number(this.data.length) + 1
+      console.log(note)
+      this.data.push(note)
+      //
+    }
+  },
+  created () {
+    this.data = [
+      { id: 1, name: 'virat', message: 'be the best you can be ' },
+      { id: 2, name: 'rohit', message: 'sun will rise again tommorrow' },
+      { id: 3, name: 'Dhoni', message: "i don't see any in em" },
+      { id: 4, name: 'kane Williamson', message: 'its me who should decide , whether i smile or cry . i prefer smile ' },
+      { id: 5, name: 'Afridi', message: ' Age is just a number' },
+      { id: 6, name: 'de villiers', message: ' ipl is harder than world cup' }
+
+    ]
+  }
+}
+</script>
+
+<style>
+*{
+  padding: 0;
+  margin: 0;
+}
+body {
+  background: #eee;
+}
+</style>
---
+++ src/components/Note.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="notes">
+    <div :key="note.id" v-for="note in data" className="note">
+      <h1>{{ note.name }}</h1>
+      <p>{{ note.message }}</p>
+      <button @click="deleteNote(note.id)"><i class="fa-solid fa-trash"></i></button>
+    </div>
+     </div>
+</template>
+
+<script>
+export default {
+  name: 'note',
+  props: {
+    data: Array
+
+  },
+  emits: ['delete-note'],
+  methods: {
+    deleteNote (id) {
+      this.$emit('delete-note', id)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+.notes{
+  margin: auto;
+  margin: 0 3rem 0 0;
+}
+.note {
+  background: #fff;
+  border-radius: 7px;
+  box-shadow: 0 2px 5px #ccc;
+  padding: 10px;
+  width: 15%;
+  margin: 1rem;
+  float: right;
+  margin-right: 5rem;
+}
+.note h1 {
+  font-size: 1.1em;
+  margin-bottom: 6px;
+}
+.note p {
+  font-size: 1.1em;
+  margin-bottom: 10px;
+  white-space: pre-wrap;
+  word-wrap: break-word;
+}
+
+.note button {
+  position: relative;
+  float: right;
+  margin-right: 10px;
+  color: #f5ba13;
+  border: none;
+  width: 36px;
+  height: 36px;
+  cursor: pointer;
+  outline: none;
+}
+.fa-trash{
+  color: #f5ba13;
+  font-size:1.5rem;
+}
+.fa-trash:hover{
+  transform: scale(1.1);
+  aspect-ratio: 1,2;
+   transition: 0.1s;
+}
+</style>
